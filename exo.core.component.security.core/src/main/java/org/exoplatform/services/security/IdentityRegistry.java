4ff79ade29e48bbfa55b2b3a9e4904f463c41eac
Dmytro Katayev <dmytro.katayev@exoplatform.com>
2009-11-04 16:33:27 +0000

    EXOJCR-224 logging level changed

--- exo.core.component.security.core/src/main/java/org/exoplatform/services/security/IdentityRegistry.java
+++ exo.core.component.security.core/src/main/java/org/exoplatform/services/security/IdentityRegistry.java
@@ -73,7 +73,7 @@ public class IdentityRegistry
       }
       catch (NullPointerException e)
       {
-         LOG.warn("Parameter " + INIT_PARAM_CONCURRENCY_LEVEL + " was not found in configuration, default "
+         LOG.info("Parameter " + INIT_PARAM_CONCURRENCY_LEVEL + " was not found in configuration, default "
             + DEFAULT_CONCURRENCY_LEVEL + "will be used.");
          return DEFAULT_CONCURRENCY_LEVEL;
       }
