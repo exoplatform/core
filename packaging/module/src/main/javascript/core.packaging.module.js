84ea0bada737589660e3be1b68d16604045037fe
Nicolas Filotto <nicolas.filotto@exoplatform.com>
2011-03-15 11:54:59 +0000

    EXOJCR-1247: Added missing dependencies

--- packaging/module/src/main/javascript/core.packaging.module.js
+++ packaging/module/src/main/javascript/core.packaging.module.js
@@ -16,23 +16,15 @@ function getModule(params) {
 
   module.component.database = 
     new Project("org.exoplatform.core", "exo.core.component.database", "jar", module.version) .
-  //addDependency(new Project("com.experlog", "xapool", "jar", "1.5.0")).
     addDependency(new Project("org.hibernate", "hibernate-core", "jar", "3.3.2.GA")).
     addDependency(new Project("org.hibernate", "hibernate-annotations", "jar", "3.4.0.GA")).
     addDependency(new Project("org.hibernate", "hibernate-commons-annotations", "jar", "3.1.0.GA")).
     addDependency(new Project("org.hibernate", "ejb3-persistence", "jar", "1.0.2.GA")).
     addDependency(new Project("javassist", "javassist", "jar", "3.4.GA")).
-   //addDependency(new Project("xdoclet", "xdoclet-hibernate-module", "jar", "1.2.3")).
-   //addDependency(new Project("xdoclet", "xdoclet", "jar", "1.2.3")).
-   //addDependency(new Project("xdoclet", "xdoclet-xdoclet-module", "jar", "1.2.3")).
-   //addDependency(new Project("xdoclet", "xjavadoc", "jar", "1.2.3")).
     addDependency(new Project("commons-collections", "commons-collections", "jar", "3.2")).
     addDependency(new Project("c3p0", "c3p0", "jar", "0.9.1.2")).
     addDependency(new Project("antlr", "antlr", "jar", "2.7.6rc1")).
     addDependency(new Project("javax.transaction", "jta", "jar", "1.0.1B")).
-   //addDependency(new Project("jotm", "jotm_jrmp_stubs", "jar", "2.0.10")).
-   //addDependency(new Project("jotm", "jotm", "jar", "2.0.10")).
-   //addDependency(new Project("howl", "howl-logger", "jar", "0.1.11")).
     addDependency(new Project("hsqldb", "hsqldb", "jar", "1.8.0.7")).
     addDependency(new Project("javax.resource", "connector-api", "jar", "1.5"));
 
@@ -47,6 +39,8 @@ function getModule(params) {
     addDependency(new Project("org.apache.poi", "poi-scratchpad", "jar", "3.6")).
     addDependency(new Project("org.apache.poi", "poi-ooxml", "jar", "3.6")).
     addDependency(new Project("org.apache.poi", "poi-ooxml-schemas", "jar", "3.6")).    
+    addDependency(new Project("org.apache.tika", "tika-core", "jar", "0.7")).    
+    addDependency(new Project("org.apache.tika", "tika-parsers", "jar", "0.7")).    
     addDependency(new Project("org.apache.xmlbeans", "xmlbeans", "jar", "2.3.0"));
     
   module.component.organization = 
