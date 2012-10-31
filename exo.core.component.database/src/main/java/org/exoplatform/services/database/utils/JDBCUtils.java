c2feec61f50848d432d0522f592e90a59e7133ff
Anatoliy Bazko <abazko@exoplatform.com>
2012-10-31 10:48:46 +0200

    COR-271: fix query for MSSQL

--- exo.core.component.database/src/main/java/org/exoplatform/services/database/utils/JDBCUtils.java
+++ exo.core.component.database/src/main/java/org/exoplatform/services/database/utils/JDBCUtils.java
@@ -91,7 +91,11 @@ public class JDBCUtils
          {
             query = "SELECT count(*) from (SELECT 1 FROM " + tableName + " FETCH FIRST 1 ROWS ONLY) T";
          }
-         else if (dialect.startsWith(DialectConstants.DB_DIALECT_MSSQL) || dialect.startsWith(DialectConstants.DB_DIALECT_SYBASE))
+         else if (dialect.startsWith(DialectConstants.DB_DIALECT_MSSQL))
+         {
+            query = "SELECT count(*) from (SELECT TOP (1) 1 FROM " + tableName + ") T";
+         }
+         else if (dialect.startsWith(DialectConstants.DB_DIALECT_SYBASE))
          {
             query = "SELECT count(*) from (SELECT TOP 1 1 FROM " + tableName + ") T";
          }
