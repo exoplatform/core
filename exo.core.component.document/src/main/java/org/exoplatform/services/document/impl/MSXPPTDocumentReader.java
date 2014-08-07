09ce58ca2f4b34cca68e6d53106aacfd996f2534
Frederic DROUET <fdrouet@exoplatform.com>
2014-08-07 16:42:07 +0200

    COR-329 : New implementation of the MSExcelDocumentReader and MSXExcelDocumentReader using the Event User Model approach to have lower memory footprint with big Excel files + Improved version of POIPropertiesReader to have a lower memory footprint with XLSX, DOCX and PPTX documents

--- exo.core.component.document/src/main/java/org/exoplatform/services/document/impl/MSXPPTDocumentReader.java
+++ exo.core.component.document/src/main/java/org/exoplatform/services/document/impl/MSXPPTDocumentReader.java
@@ -18,11 +18,11 @@
  */
 package org.exoplatform.services.document.impl;
 
+import org.apache.poi.POIXMLProperties;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.exceptions.OpenXML4JException;
 import org.apache.poi.openxml4j.exceptions.OpenXML4JRuntimeException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
-import org.apache.poi.xslf.XSLFSlideShow;
 import org.apache.poi.xslf.extractor.XSLFPowerPointExtractor;
 import org.apache.xmlbeans.XmlException;
 import org.exoplatform.commons.utils.SecurityHelper;
@@ -171,43 +171,24 @@ public class MSXPPTDocumentReader extends BaseDocumentReader
     */
    public Properties getProperties(final InputStream is) throws IOException, DocumentReadException
    {
-      final POIPropertiesReader reader = new POIPropertiesReader();
-      try
-      {
-         SecurityHelper.doPrivilegedExceptionAction(new PrivilegedExceptionAction<Void>()
-         {
-            public Void run() throws Exception
-            {
-               reader.readDCProperties(new XSLFSlideShow(OPCPackage.open(is)));
-               return null;
-            }
-         });
+      try {
+         OPCPackage container = SecurityHelper
+             .doPrivilegedIOExceptionAction(new PrivilegedExceptionAction<OPCPackage>() {
+                public OPCPackage run() throws Exception {
+                   return OPCPackage.open(is);
+                }
+             });
+         POIXMLProperties xmlProperties = new POIXMLProperties(container);
+         POIPropertiesReader reader = new POIPropertiesReader();
+         reader.readDCProperties(xmlProperties);
+         return reader.getProperties();
+      } catch (InvalidFormatException e) {
+         throw new DocumentReadException("The format of the document to read is invalid.", e);
+      } catch (XmlException e) {
+         throw new DocumentReadException("Problem during the document parsing.", e);
+      } catch (OpenXML4JException e) {
+         throw new DocumentReadException("Problem during the document parsing.", e);
       }
-      catch (PrivilegedActionException pae)
-      {
-         Throwable cause = pae.getCause();
-         if (cause instanceof InvalidFormatException)
-         {
-            throw new DocumentReadException("Can't read properties from OOXML document", cause);
-         }
-         else if (cause instanceof OpenXML4JException)
-         {
-            throw new DocumentReadException("Can't read properties from OOXML document", cause);
-         }
-         else if (cause instanceof XmlException)
-         {
-            throw new DocumentReadException("Can't read properties from OOXML document", cause);
-         }
-         else if (cause instanceof RuntimeException)
-         {
-            throw (RuntimeException)cause;
-         }
-         else
-         {
-            throw new RuntimeException(cause);
-         }
-      }
-      return reader.getProperties();
    }
 
 }
