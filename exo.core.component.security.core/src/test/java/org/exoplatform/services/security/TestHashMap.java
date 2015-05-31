4dcc7431b8a7dcbb4d52a5f0b19d99a4539f31ae
aboughzela <aboughzela@exoplatform.com>
2015-05-31 22:05:30 +0100

    COR-347: Fix TestGroupHandler#testAddDuplicateChild

---
+++ exo.core.component.security.core/src/test/java/org/exoplatform/services/security/TestHashMap.java
@@ -0,0 +1,16 @@
+package org.exoplatform.services.security;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import junit.framework.TestCase;
+
+
+
+public class TestHashMap extends TestCase {
+   public void testHashMap() throws Exception {
+
+   }
+}
