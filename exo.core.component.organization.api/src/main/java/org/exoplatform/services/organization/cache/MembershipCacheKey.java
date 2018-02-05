6aa4121fc2b39c7c0a51aa11f7a312eb2f39d938
Boubaker Khanfir <bkhanfir@exoplatform.com>
2018-02-05 15:21:51 +0100

    EXOGTN-2319 Optimize Organization Service cache for cluster (#27)

--- exo.core.component.organization.api/src/main/java/org/exoplatform/services/organization/cache/MembershipCacheKey.java
+++ exo.core.component.organization.api/src/main/java/org/exoplatform/services/organization/cache/MembershipCacheKey.java
@@ -31,85 +31,86 @@ import java.io.Serializable;
 /**
  * Will be used as key for cacheMembership.
  */
-public class MembershipCacheKey implements Serializable
-{
-   /**
-    * serialVersionUID.
-    */
-   private static final long serialVersionUID = -188435911917156440L;
+public class MembershipCacheKey implements Serializable {
+  /**
+   * serialVersionUID.
+   */
+  private static final long serialVersionUID = -188435911917156440L;
 
-   private final String userName;
+  private final String      userName;
 
-   private final String groupId;
+  private final String      groupId;
 
-   private final String type;
+  private final String      type;
 
-   private final int hashcode;
+  private final int         hashcode;
 
-   public MembershipCacheKey(String userName, String groupId, String type)
-   {
-      this.userName = userName;
-      this.groupId = groupId;
-      this.type = type;
+  public MembershipCacheKey(String userName, String groupId, String type) {
+    this.userName = userName;
+    this.groupId = groupId;
+    this.type = type;
 
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ((groupId == null) ? 0 : groupId.hashCode());
-      result = prime * result + ((type == null) ? 0 : type.hashCode());
-      hashcode = prime * result + ((userName == null) ? 0 : userName.hashCode());
-   }
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((groupId == null) ? 0 : groupId.hashCode());
+    result = prime * result + ((type == null) ? 0 : type.hashCode());
+    hashcode = prime * result + ((userName == null) ? 0 : userName.hashCode());
+  }
 
-   public MembershipCacheKey(Membership m)
-   {
-      this.userName = m.getUserName();
-      this.groupId = m.getGroupId();
-      this.type = m.getMembershipType();
+  public MembershipCacheKey(Membership m) {
+    this.userName = m.getUserName();
+    this.groupId = m.getGroupId();
+    this.type = m.getMembershipType();
 
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ((groupId == null) ? 0 : groupId.hashCode());
-      result = prime * result + ((type == null) ? 0 : type.hashCode());
-      hashcode = prime * result + ((userName == null) ? 0 : userName.hashCode());
-   }
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((groupId == null) ? 0 : groupId.hashCode());
+    result = prime * result + ((type == null) ? 0 : type.hashCode());
+    hashcode = prime * result + ((userName == null) ? 0 : userName.hashCode());
+  }
 
-   @Override
-   public int hashCode()
-   {
-      return hashcode;
-   }
+  public String getGroupId() {
+    return groupId;
+  }
 
-   @Override
-   public boolean equals(Object obj)
-   {
-      if (this == obj)
-         return true;
-      if (obj == null)
-         return false;
-      if (getClass() != obj.getClass())
-         return false;
-      MembershipCacheKey other = (MembershipCacheKey)obj;
-      if (groupId == null)
-      {
-         if (other.groupId != null)
-            return false;
-      }
-      else if (!groupId.equals(other.groupId))
-         return false;
-      if (type == null)
-      {
-         if (other.type != null)
-            return false;
-      }
-      else if (!type.equals(other.type))
-         return false;
-      if (userName == null)
-      {
-         if (other.userName != null)
-            return false;
-      }
-      else if (!userName.equals(other.userName))
-         return false;
+  public String getType() {
+    return type;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  @Override
+  public int hashCode() {
+    return hashcode;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
       return true;
-   }
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    MembershipCacheKey other = (MembershipCacheKey) obj;
+    if (groupId == null) {
+      if (other.groupId != null)
+        return false;
+    } else if (!groupId.equals(other.groupId))
+      return false;
+    if (type == null) {
+      if (other.type != null)
+        return false;
+    } else if (!type.equals(other.type))
+      return false;
+    if (userName == null) {
+      if (other.userName != null)
+        return false;
+    } else if (!userName.equals(other.userName))
+      return false;
+    return true;
+  }
 
 }
